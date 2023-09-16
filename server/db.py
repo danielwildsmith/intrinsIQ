d7809f85b25577a634e3d1010279b2cde37ba138
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-09-16 01:58:19 -0400

    feat: connect sqlite db

---
+++ server/db.py
@@ -0,0 +1,17 @@
+from peewee import Model, SqliteDatabase, CharField, BooleanField
+
+# Define an SQLite database connection
+db = SqliteDatabase('mydatabase.db')
+
+# Define your model(s)
+class Task(Model):
+    title = CharField()
+    completed = BooleanField()
+
+    class Meta:
+        database = db  # Assign the database connection to the model
+
+db.connect()
+print('Connected to db.')
+db.create_tables([Task], safe=True)
+db.close()
