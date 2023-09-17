59630013fa3f53096a8a0fa993ad9ffac33e3621
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-09-17 05:23:54 -0400

    fix: intrinsic value line

--- client/components/graph.tsx
+++ client/components/graph.tsx
@@ -8,7 +8,7 @@ export interface StockDayData {
 }
 
 export interface IntrinsicData {
-  value: Number;
+  intrinsicValue: Number;
 }
 
 export const RangeChart = ({company}: {company: string}) => {
@@ -34,7 +34,6 @@ export const RangeChart = ({company}: {company: string}) => {
     }
   };
 
-
   useEffect(() => {
     getStockData();
     GetIntrinsicData();
@@ -42,7 +41,6 @@ export const RangeChart = ({company}: {company: string}) => {
 
   useEffect(() => {
     if (stockData && chartContainerRef.current && intrinsicData) {
-      console.log(stockData)
       console.log(intrinsicData)
       
       const chart = createChart(chartContainerRef.current, { width: 800, height: 400, grid: {
@@ -63,27 +61,24 @@ export const RangeChart = ({company}: {company: string}) => {
       }
     }
  );
- const intrinsicSeries = chart.addAreaSeries({
-  topColor: 'rgba(51, 215, 120, 1)',
-  bottomColor: 'rgba(0, 0, 0, 0.0)',
-  lineColor: 'rgba(51, 215, 120, 1)',
+ const intrinsicSeries = chart.addLineSeries({
+  color: 'rgba(255, 192, 0, 0.7)',
   lineWidth: 3,
-  lineStyle: 0
+  lineStyle: 0,
+  priceLineVisible: false,
+  lastValueVisible: false
 });
 
-      const areaSeries = chart.addAreaSeries({lineColor: 'rgba(33, 150, 243, 1)', topColor: 'rgba(33, 150, 243, 0.56)', bottomColor: 'rgba(0, 0, 0, 0.0)'});
+      const areaSeries = chart.addAreaSeries({lineColor: 'rgba(33, 150, 243, 1)', topColor: 'rgba(33, 150, 243, 0.56)', bottomColor: 'rgba(0, 0, 0, 0.0)', priceLineVisible: false, lastValueVisible: false});
 
       const formattedData = stockData.map(item => {
         return { time: item.date, value: Number(item.price) }; // assuming item.day is time and item.price is the desired value
       });
 
-      const intrinsicDataRangeChart = intrinsicData.map(item => {
-        return { time: '2023-09-15', value: item.value }; // Here we take the midpoint as value
-      });
-  
-
       areaSeries.setData(formattedData);
-      intrinsicSeries.setData(intrinsicDataRangeChart);
+      console.log(intrinsicData[0].intrinsicValue)
+      //@ts-ignore
+      intrinsicSeries.setData([{time: '2018-09-15', value: intrinsicData[0].intrinsicValue}, {time: '2023-09-15', value: intrinsicData[0].intrinsicValue}]);
 
       return () => chart.remove()
     }
