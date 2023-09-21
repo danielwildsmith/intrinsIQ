306e74655bab1ca79a796bd723ecf6a4a7aab9f6
Daniel Wildsmith <danielwildsmith@ufl.edu>
2023-09-21 17:54:59 -0400

    fix: update connectDB func in main

--- server/main.py
+++ server/main.py
@@ -1,13 +1,13 @@
-from sp500 import getCompanies
-from intrinsic_value import calculateIntrinsicValue
 from flask import Flask, jsonify
 from db import connectDB, db, StockPrices, IntrinsicValues
 from playhouse.shortcuts import model_to_dict
 from flask_cors import CORS
+from seed import seedDB
 
 app = Flask(__name__)
 cors = CORS(app)
-connectDB(db)
+connectDB()
+# seedDB()
 
 @app.route('/stock/<company>', methods=['GET'])
 async def getStock(company):
@@ -79,24 +79,3 @@ if __name__ == '__main__':
     app.run()
 
 db.close()
-
-# companies = getCompanies()
-# companies.remove('MNST')
-# companies.remove('MPWR')
-# companies.remove('ISRG')
-# # some companies have different column names and giving us problems...
-# # ALL, AXP
-
-# # 33 AWK is -206.11
-# # 43 AAPL is 104.38
-# count = 1
-# for company in companies:
-#     intrinsicValue = calculateIntrinsicValue(company)
-    
-#     print(count,company, ': ', intrinsicValue)
-#     newRow = IntrinsicValues.create(company=company, intrinsicValue=intrinsicValue)
-#     newRow.save()
-#     count += 1
-
-# # Used to load the database with the stock prices over the last 5 years:
-# #print(loadStockPriceData(getCompanies()))
